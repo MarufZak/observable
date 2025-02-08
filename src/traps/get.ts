a0a6400197058e30d7138b08bc2a16bfcacff2b0
Ma'ruf <marufzak04@gmail.com>
2025-02-08 17:42:31 +0500

    fix: bug

--- src/traps/get.ts
+++ src/traps/get.ts
@@ -1,8 +1,8 @@
 import type { TKey, TObserver, TSubject, TValue } from "../types.js";
 
 export const createGetTrap = (observer: TObserver) => {
-    return (target: TSubject, key: TKey, value: TValue) => {
-        observer({ type: "get", key, value });
+    return (target: TSubject, key: TKey) => {
+        observer({ type: "get", key, value: target[key] });
         return target[key as string];
     };
 };
