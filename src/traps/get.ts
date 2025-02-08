848e4f3ec506e77644c567b979204a0f191b071b
Ma'ruf <marufzak04@gmail.com>
2025-02-08 20:34:47 +0500

    chore: edits

--- src/traps/get.ts
+++ src/traps/get.ts
@@ -3,6 +3,6 @@ import type { TKey, TObserver, TSubject, TValue } from "../types.js";
 export const createGetTrap = (observer: TObserver) => {
     return (target: TSubject, key: TKey) => {
         observer({ type: "get", key, value: target[key] });
-        return target[key as string];
+        return target[key];
     };
 };
