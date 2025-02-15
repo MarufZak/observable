1b07f1bc9f37fd611d354cdd4e290f215baab766
Ma'ruf <marufzak04@gmail.com>
2025-02-15 14:39:27 +0500

    feat: recursive delete trap

--- src/traps/deleteProperty.ts
+++ src/traps/deleteProperty.ts
@@ -1,10 +1,16 @@
-import type { TKey, TObserver, TSubject } from "../types";
+import type { TKey, TObserver, TPath, TSubject } from "../types";
+import { getObjectTrace } from "../utils";
 
-export const createDeletePropertyTrap = (observer: TObserver) => {
+export const createDeletePropertyTrap = (observer: TObserver, path: TPath) => {
     return (target: TSubject, key: TKey) => {
         const isPresent = key in target;
+        const value = target[key];
         if (isPresent) delete target[key];
-        observer({ type: "delete", key, value: target[key] });
+        observer({
+            type: "delete",
+            key: getObjectTrace(path, key),
+            value,
+        });
 
         return isPresent;
     };
--- src/traps/index.ts
+++ src/traps/index.ts
@@ -6,7 +6,7 @@ import { createDeletePropertyTrap } from "./deleteProperty";
 export const getTraps = (observer: TObserver, path: TPath) => {
     const get = createGetTrap(observer, path);
     const set = createSetTrap(observer, path);
-    const deleteProperty = createDeletePropertyTrap(observer);
+    const deleteProperty = createDeletePropertyTrap(observer, path);
 
     return {
         get,
