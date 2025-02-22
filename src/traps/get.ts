5bb20c44a983f187f6e0237635c09ee4956a2d5f
Ma'ruf <marufzak04@gmail.com>
2025-02-22 19:24:33 +0500

    feat: add note and fix bug

--- src/traps/get.ts
+++ src/traps/get.ts
@@ -3,12 +3,20 @@ import { getObjectTrace } from "../utils";
 
 export const createGetTrap = (observer: TObserver, path: TPath) => {
     return (target: TSubjectObject, key: TKey, receiver: TReceiver) => {
+        const value = Reflect.get(target, key, receiver);
+
         observer({
             type: "get",
             key: getObjectTrace(path, key),
-            value: target[key],
+            value,
         });
 
-        return Reflect.get(target, key, receiver);
+        // Map uses its internal slot with this.[[MapData]] with methods
+        // like Map.prototype.(get|set), but because `this` is Proxy, it
+        // ends up with error. We might explicitly bind the context to original
+        // object, but we won't, because we want to keep original proxying without
+        // overhead. Same with other builtin objects, like Set, Date private fields
+        // in classes with #, because they also use private fields.
+        return value;
     };
 };
